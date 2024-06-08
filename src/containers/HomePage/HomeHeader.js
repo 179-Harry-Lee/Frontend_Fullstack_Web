5b80cd5fbc087cace68938fc949d0be212a58284
Wesley Dung <ducdung17903@gmail.com>
2024-06-08 20:49:55 +0700

    Complete select language  VI/EN

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,8 +3,18 @@ import { connect } from "react-redux";
 import "./HomeHeader.scss";
 import logo from "../../assets/logo.png";
 import { FormattedMessage } from "react-intl";
+import { LANGUAGES } from "../../utils/constant";
+import { changeLanguageApp } from "../../store/actions/appActions";
+
 class HomeHeader extends Component {
+  changeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+    //fire redux event : actions
+  };
+
   render() {
+    let language = this.props.language;
+    console.log("check language", language);
     return (
       <React.Fragment>
         <div className="home-header-container">
@@ -66,8 +76,28 @@ class HomeHeader extends Component {
                 <i className="fas fa-question-circle"></i>{" "}
                 <FormattedMessage id="homeheader.support" />
               </div>
-              <div className="language-vi">VN</div>
-              <div className="language-en">EN</div>
+              <div
+                className={
+                  language === LANGUAGES.VI
+                    ? "language-vi active"
+                    : "language-vi"
+                }
+              >
+                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                  VN
+                </span>
+              </div>
+              <div
+                className={
+                  language === LANGUAGES.EN
+                    ? "language-en active"
+                    : "language-en"
+                }
+              >
+                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                  EN
+                </span>
+              </div>
             </div>
           </div>
         </div>
@@ -150,7 +180,9 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
