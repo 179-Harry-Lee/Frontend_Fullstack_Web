4416d41994a4823875d18ce919cc4782623dd719
Wesley Dung <ducdung17903@gmail.com>
2024-07-11 01:00:09 +0700

    11/7/2024

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,6 +26,7 @@ import CustomScrollbars from "../components/CustomScrollbars";
 import DetailDoctor from "./Patient/Doctor/DetailDoctor";
 
 import Doctor from "../routes/Doctor";
+import VerifyEmail from "./Patient/VerifyEmail";
 
 class App extends Component {
   handlePersistorState = () => {
@@ -69,6 +70,10 @@ class App extends Component {
                   />
                   <Route path={path.HOMEPAGE} component={HomePage} />
                   <Route path={path.DETAIL_DOCTOR} component={DetailDoctor} />
+                  <Route
+                    path={path.VERIFY_EMAIL_BOOKING}
+                    component={VerifyEmail}
+                  />
                 </Switch>
               </CustomScrollbars>
             </div>
--- src/services/userService.js
+++ src/services/userService.js
@@ -69,6 +69,10 @@ const postPatientBookAppointment = (data) => {
   return axios.post("/api/patient-book-appointment", data);
 };
 
+const postVerifyBookAppointment = (data) => {
+  return axios.post("/api/verify-book-appointment", data);
+};
+
 export {
   handleLoginApi,
   getAllUsers,
@@ -85,4 +89,5 @@ export {
   getExtraInforDoctorById,
   getProfileDoctorById,
   postPatientBookAppointment,
+  postVerifyBookAppointment,
 };
