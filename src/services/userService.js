8c795082d87a0e42125514ea32ac51eb4e83a380
Wesley Dung <ducdung17903@gmail.com>
2024-07-11 16:30:45 +0700

    11/7/2024

--- src/services/userService.js
+++ src/services/userService.js
@@ -73,6 +73,10 @@ const postVerifyBookAppointment = (data) => {
   return axios.post("/api/verify-book-appointment", data);
 };
 
+const createNewSpecialty = (data) => {
+  return axios.post("/api/create-new-specialty", data);
+};
+
 export {
   handleLoginApi,
   getAllUsers,
@@ -90,4 +94,5 @@ export {
   getProfileDoctorById,
   postPatientBookAppointment,
   postVerifyBookAppointment,
+  createNewSpecialty,
 };
